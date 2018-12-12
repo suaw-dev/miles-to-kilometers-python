a48d897ef5ed6c4492eba6c51c3147ec0789a244
suaw <35884411+suaw-dev@users.noreply.github.com>
2018-12-11 18:06:14 -0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,14 @@
+# Problem : Receive miles and convert to kilometers
+# kilometers = miles * 1.60934
+
+# Enter Miles
+
+miles = int(input("Number of miles you would like to convert: "))
+
+# Convert miles to kilometers
+
+kilometers = miles * 1.60934
+
+# Print result
+
+print(miles, 'miles is', kilometers, 'kilometers')
